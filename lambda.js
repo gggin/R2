f4ca440b4bcb9ef3be935c7a645adc2153936c91
Gin Liu <fm369o802340@163.com>
2016-06-02 17:58:29 +0800

    fix lexcial scoping

--- lambda.js
+++ lambda.js
@@ -8,23 +8,38 @@ var code2 = [['lambda', ['y'], ['-', 'y', code]], 9];
 var code3 = 
 [['lambda', ['x'], ['x', 1]], ['lambda', ['y'], ['+', 'a', ['+', 'y', 5]]]]
 
+/*
+(let ([x 2])
+  (let ([f (lambda (y) (* x y))])
+    (let ([x 4])
+      (f 3))))
+*/
+
+var code4 = 
+['let', ['x', 2],
+    ['let', ['f', ['lambda', ['y'], ['*', 'x', 'y']]],
+        ['let', ['x', 4],
+            ['f', 3]
+        ]
+    ]
+];
+
 var index = 0;
 var lambdaScopeName = "LAMBDA_";
+var letScopeName = "LET_";
 
 var env = {'a' : 5};
-var currentEnv;
 
-currentEnv = env;
-function intep(code) {
+function intep(code, cuEnv) {
     console.log('-------BEGIN---')
     console.log(code);
     console.log('---------------')
     console.log(env);
     console.log('---------------')
-    console.log(currentEnv);
+    console.log(cuEnv);
     console.log('-------END-----')
     if (typeof code === 'string') {
-        var tt = currentEnv;
+        var tt = cuEnv;
         var re = tt[code];
         while (re === undefined) {
             tt = tt._p;
@@ -37,29 +52,39 @@ function intep(code) {
 
     if (code.length == 3) {
         if (code[0] === 'lambda') {
+            code.env = cuEnv;
             return code;
-        }else if (code[0] === '+') {
-            return intep(code[1]) + intep(code[2]);        
+        } else if(code[0] === 'let') {
+            index++;
+            var tmpScope = cuEnv[letScopeName + index] = {};
+            var origin = cuEnv;
+            cuEnv = tmpScope;
+            cuEnv["_p"] = origin;
+            tmpScope[code[1][0]] = intep(code[1][1], cuEnv);
+            return intep(code[2], cuEnv);
+        } else if (code[0] === '+') {
+            return intep(code[1], cuEnv) + intep(code[2], cuEnv);        
         } else if (code[0] === '-') {
-            return intep(code[1]) - intep(code[2]);        
+            return intep(code[1], cuEnv) - intep(code[2], cuEnv);        
         } else if (code[0] === '*') {
-            return intep(code[1]) * intep(code[2]);        
+            return intep(code[1], cuEnv) * intep(code[2], cuEnv);        
         } else if (code[0] === '/') {
-            return intep(code[1]) / intep(code[2]);        
+            return intep(code[1], cuEnv) / intep(code[2], cuEnv);        
         } else {
             throw new Error;
         }
     } else if (code.length == 2) {
-        var left = intep(code[0]);
-        var right = intep(code[1]);
+        var left = intep(code[0], cuEnv);
+        var right = intep(code[1], cuEnv);
         index++;
-        var tmpScope = currentEnv[lambdaScopeName + index] = {};
-        var origin = currentEnv;
-        currentEnv = tmpScope;
-        currentEnv["_p"] = origin;
+        var ttenv = left.env;
+        var tmpScope = ttenv[lambdaScopeName + index] = {};
+        var origin = ttenv;
+        ttenv = tmpScope;
+        ttenv["_p"] = origin;
         var varName = left[1][0];
         tmpScope[varName] = right;
-        return intep(left[2]);
+        return intep(left[2], ttenv);
     } else {
         throw new Error;
     }
@@ -68,6 +93,6 @@ function intep(code) {
 //var result = intep(code2);
 //console.log(result);
 
-console.log(code3);
-var result = intep(code3);
+console.log(code4);
+var result = intep(code4, env);
 console.log(result);
